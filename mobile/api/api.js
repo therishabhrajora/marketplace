def3b90d7abc0d42221ea232469133e2ece5c418
therishabhrajora <rishabhrajora20gmail.com>
2026-02-20 17:55:44 +0530

    update

---
+++ mobile/api/api.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+import axios from "axios";
+
+const API = axios.create({
+  // baseURL: "http://10.190.246.100:5000",
+  baseURL: "https://webapp-4ncg.onrender.com",
+  
+});
+
+// attach token automatically
+API.interceptors.request.use((req) => {
+  if (global.token) {
+    req.headers.Authorization = `Bearer ${global.token}`;
+  }
+  return req;
+});
+
+export default API;
+=======
+import axios from "axios";
+
+const API = axios.create({
+  // baseURL: "http://10.190.246.100:5000",
+  baseURL: "https://webapp-4ncg.onrender.com",
+  
+});
+
+// attach token automatically
+API.interceptors.request.use((req) => {
+  if (global.token) {
+    req.headers.Authorization = `Bearer ${global.token}`;
+  }
+  return req;
+});
+
+export default API;
+>>>>>>> 743fede57b4879a92b111025dce751ef468c919a
